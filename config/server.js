b76b067085f49f2ce3029214038e6c5963d828e3
Triston  Carter <taekondainc.gmail.com>
2021-07-27 02:32:05 -0400

    backend

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'aaf1a82013762d2e9f4449456e726dd2'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e86a87b9-3924-40eb-bb2b-476a673e5de3'
+};
